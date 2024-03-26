57cc9361e330ae1d5a252e9c492817fe59467108
Luisma <luismasuarezzz@gmail.com>
2024-03-26 12:33:47 -0400

    Basic condiguration for build ready

--- src/core/components/index.ts
+++ src/core/components/index.ts
@@ -1,7 +1,6 @@
 export {default as Vector} from './Vector/Vector';
-export {default as Section} from './Section/Section';
-export * from './index';
 export {default as Typography} from './Typography/Typography';
-export {default as Input} from './Input/Input';
+export {default as Section} from './Section/Section';
 export {default as Box} from './Box/Box';
+export {default as Input} from './Input/Input';
 export {default as Button} from './Button/Button';
